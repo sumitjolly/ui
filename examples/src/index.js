0bc96ae0a21c01c6733418498aa61143ba055ed3
Hugo Nogueira <hugomn@gmail.com>
2018-06-19 17:56:42 +0200

    feat: Initial commit

---
+++ examples/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "react-dom";
+import Checkbox from "../../src";
+
+const App = () => <Checkbox />;
+
+render(<App />, document.getElementById("root"));
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Checkbox as SUICheckbox } from "@infinitecsolutions/semantic-ui-react";
+
+const Checkbox = () => {
+  return <SUICheckbox checked={true} />;
+};
+
+export default Checkbox;
