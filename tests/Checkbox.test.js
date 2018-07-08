ca849fcd42f32f725a2dd0f329d4b8c57b77373b
Hugo Nogueira <hugomn@gmail.com>
2018-07-09 00:30:42 +0200

    feat: Rollback babel to v6, add tests to Checkbox and Divider

--- tests/Checkbox.test.js
+++ tests/Checkbox.test.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { Checkbox } from "../src/components";
+import { shallow } from "enzyme";
 
 describe("Checkbox", () => {
-  it("renders a checkbox by default", () => {
-    expect(true).toBeTruthy();
+  const SUICheckbox = shallow(<Checkbox label="Test" />).dive();
+
+  it("should render an element of type 'div'", () => {
+    expect(SUICheckbox.is("div")).toBeTruthy();
+  });
+
+  it("should countain the classes 'ui' and 'checkbox'", () => {
+    expect(SUICheckbox.hasClass("ui")).toBeTruthy();
+    expect(SUICheckbox.hasClass("checkbox")).toBeTruthy();
+  });
+
+  it("should render the specified text as children", () => {
+    expect(SUICheckbox.find("label").text()).toEqual("Test");
   });
 });
