1c6c17e09a4541190d088dbbeb7ffee618b1b310
Hugo Nogueira <hugomn@gmail.com>
2018-09-05 10:40:40 +0200

    chore: Update docs theme to finavi

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     rules: [
       {
         oneOf: [
-          getSemanticLessRule("beesy"),
+          getSemanticLessRule("finavi"),
           {
             test: /\.css$/i,
             use: [MiniCssExtractPlugin.loader, "css-loader"]
